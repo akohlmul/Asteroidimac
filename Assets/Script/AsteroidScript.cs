c2a83ac0208b1d514b8f64078999d8fb826ad984
Amandine <akohlmuller@gmail.com>
2020-04-23 12:53:03 +0200

    Assets

---
+++ Assets/Script/AsteroidScript.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+
+	public float maxThrust;
+	public float maxTorque;
+	public Rigidbody2D rb;
+	public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+    public int asteroidSize; //3 = Large, 2 = Medium, 1 = Small
+    public GameObject asteroidMedium;
+    public GameObject asteroidSmall;
+    public int points;
+    public GameObject player;
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	//Add a random amount of torque and thrust to the asteroid
+    	Vector2 thrust = new Vector2(Random.Range(-maxThrust, maxThrust),Random.Range(-maxThrust, maxThrust));
+    	float torque = Random.Range(-maxTorque, maxTorque);
+
+    	rb.AddForce(thrust);
+    	rb.AddTorque(torque);
+
+        // find the player
+        player = GameObject.FindWithTag("Player");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//Screen wraping
+        Vector2 newPos = transform.position;
+        if(transform.position.y > screenTop) {
+            newPos.y = screenBottom;
+        }
+        if(transform.position.y < screenBottom) {
+            newPos.y = screenTop;
+        }
+        if(transform.position.x > screenRight) {
+            newPos.x = screenLeft;
+        }
+        if(transform.position.x < screenLeft) {
+            newPos.x = screenRight;
+        }
+
+        transform.position = newPos;
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // check to see if its a bullet
+        if(other.CompareTag("bullet")) {
+            //destroy the bullet
+            Destroy(other.gameObject);
+            //check the size of the asteroid and spawn in the next smaller size
+            if(asteroidSize == 3) {
+                //Spawn two medium asteroids
+                Instantiate(asteroidMedium, transform.position, transform.rotation);
+                Instantiate(asteroidMedium, transform.position, transform.rotation);
+            }
+            else if(asteroidSize == 2) {
+                //Spawn two small asteroids
+                Instantiate(asteroidSmall, transform.position, transform.rotation);
+                Instantiate(asteroidSmall, transform.position, transform.rotation);
+            }
+            else if(asteroidSize == 1) {
+                //Remove the asteroid
+            }
+
+            // tell the player to score come points
+            player.SendMessage("ScorePoints", points);
+
+            // make an explosion
+            GameObject newExplosion = Instantiate(explosion,transform.position,transform.rotation);
+            Destroy(newExplosion, 3f);
+
+            // remove the current asteroid
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/FighterControls.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FighterControls : MonoBehaviour
+{
+
+	public Rigidbody2D rb;
+	public float thrust;
+	public float turnThrust;
+	private float thrustInput;
+	private float turnInput;
+    public float screenTop;
+    public float screenBottom;
+    public float screenLeft;
+    public float screenRight;
+    public float bulletForce;
+    public float deathForce;
+
+    public int score;
+    public int lives;
+
+    public Text scoreText;
+    public Text livesText;
+
+    public AudioSource audio;
+
+    public GameObject explosion;
+    public GameObject bullet;
+
+    public Color inColor;
+    public Color normalColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+
+        scoreText.text = "Score " + score;
+        livesText.text = "Lives " + lives;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	// Get input from the keyboard and apply thrust
+    	//Check for input from the keyboard
+    	thrustInput = Input.GetAxis("Vertical");
+    	turnInput = Input.GetAxis("Horizontal");
+
+        //Check for input from the fire key and make bullets
+        if(Input.GetButtonDown("Fire1")) {
+            GameObject newBullet = Instantiate(bullet, transform.position, transform.rotation);
+            newBullet.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * bulletForce);
+            Destroy(newBullet, 5.0f);
+        }
+
+        //rotate the ship
+        transform.Rotate(Vector3.forward * turnInput * Time.deltaTime * -turnThrust);
+
+        //Screen wraping
+        Vector2 newPos = transform.position;
+        if(transform.position.y > screenTop) {
+            newPos.y = screenBottom;
+        }
+        if(transform.position.y < screenBottom) {
+            newPos.y = screenTop;
+        }
+        if(transform.position.x > screenRight) {
+            newPos.x = screenLeft;
+        }
+        if(transform.position.x < screenLeft) {
+            newPos.x = screenRight;
+        }
+
+        transform.position = newPos;
+
+    }
+
+    void FixedUpdate() 
+    {
+    	rb.AddRelativeForce(Vector2.up * thrustInput * thrust);
+        //rb.AddTorque(-turnInput);
+    }
+
+    void ScorePoints(int pointsToAdd) {
+        score += pointsToAdd;
+        scoreText.text = "Score " + score;
+    }
+
+    void Respawn() {
+        rb.velocity = Vector2.zero;
+        transform.position = Vector2.zero;
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        sr.enabled = true;
+        sr.color = inColor;
+        Invoke("Invunerable", 3f);
+    }
+
+    void Invunerable() {
+        GetComponent<Collider2D>().enabled = true;
+        GetComponent<SpriteRenderer>().color = normalColor;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log(col.relativeVelocity.magnitude);
+
+        if(col.relativeVelocity.magnitude > deathForce) {
+            lives--;
+            //make explosion
+            GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(newExplosion, 3f);
+            livesText.text = "Lives " + lives;
+
+            //Respawn New Life
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Collider2D>().enabled = false;
+            Invoke("Respawn", 3f);
+            
+            if(lives <= 0) {
+                GameOver();
+            }
+        } else {
+            audio.Play ();
+        }
+    }
+
+    void GameOver() {
+        CancelInvoke();
+    }
+}
